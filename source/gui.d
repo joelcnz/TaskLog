959a351bdf99a5c4b76ebe1cbc442effdb83efbf
Joel Christensen <joelcnz@gmail.com>
2018-01-11 12:06:20 +1300

    Hack popup and other stuff

--- source/gui.d
+++ source/gui.d
@@ -34,55 +34,64 @@ public:
         _control.setup(_taskMan);
 
 		_window = Platform.instance.createWindow(
-			"GUI TimeLog Program", null, WindowFlag.Resizable, 1280, 800);
+			"Time Log", null, WindowFlag.Resizable, 1280, 800);
 
 		// Crease widget to show in window
 		_window.mainWidget = parseML(q{
 			HorizontalLayout {
-				VerticalLayout {
-					margins: 3
-					padding: 3
+				//#0000FF #C0E0E070
+				backgroundColor: "#7070FF0C" // Lime green
+				HorizontalLayout {
+					VerticalLayout {
+						margins: 3
+						padding: 3
 
-					EditBox {
-						id: editBoxMain
-						minWidth: 1100; minHeight: 700; maxHeight: 640;
-					}
+						EditBox {
+							id: editBoxMain
+							minWidth: 1100; minHeight: 700; maxHeight: 640;
+						}
 
-					Button { id: buttonActivate; maxWidth: 100; text: "- Activate Last Line -" }
+						Button { id: buttonActivate; maxWidth: 100; text: "- Activate Last Line -" }
 
-					HorizontalLayout {
-						TextWidget { text: "Category:" }
-						TextWidget {
-							id: textWidgetCategory
-							text: "(none set)"
+						HorizontalLayout {
+							TextWidget { text: "Category:" }
+							TextWidget {
+								id: textWidgetCategory
+								text: "(none set)"
+							}
+						}
+
+						HorizontalLayout {
+							TextWidget { text: "Comment:" }
+							EditLine {
+								id: editLineComment
+								minWidth: 900; maxWidth: 900
+								text: ""
+							}
+
+							Button { id: buttonTest; text: "*Command"; }
 						}
 					}
-
-					HorizontalLayout {
-						TextWidget { text: "Comment:" }
-						EditLine {
-							id: editLineComment
-							minWidth: 900; maxWidth: 900
-							text: ""
+					VerticalLayout {
+						HorizontalLayout {
+							TextWidget { text: "Ref:" }
+							EditLine { id: editLineId; text: "0"; minWidth: 100; maxWidth: 100 }
 						}
-					}
-				}
-				VerticalLayout {
-					HorizontalLayout {
-						TextWidget { text: "Ref:" }
-						EditLine { id: editLineId; text: "0"; minWidth: 100; maxWidth: 100 }
-					}
 
-					Button { id: buttonGet; maxWidth: 100; text: "Get" }
+						Button { id: buttonGet; maxWidth: 100; text: "Get" }
 
-					TextWidget { text: "Date:" }
-					EditLine { id: editLineDate; text: "1 1 2018"; minWidth: 100; maxWidth: 100 }
-					TextWidget { text: "Time:" }
-					HorizontalLayout { EditLine { id: editLineTime; text: "0 0 0"; minWidth: 100; maxWidth: 100 } CheckBox { id: checkBoxTime; } }
-					TextWidget { text: "End Time:" }
-					HorizontalLayout { EditLine { id: editLineEndTime; text: "0 0 0"; minWidth: 100; maxWidth: 100 } CheckBox { id: checkBoxEndTime; } }
-					TextWidget { text: "Duration:" }
-					EditLine { id: editLineDuration; text: "0 0 0"; minWidth: 100; maxWidth: 100 }
+						TextWidget { text: "Date:" }
+						EditLine { id: editLineDate; text: "1 1 2018"; minWidth: 100; maxWidth: 100 }
+						TextWidget { text: "Time:" }
+						HorizontalLayout { EditLine { id: editLineTime; text: "0 0 0"; minWidth: 100; maxWidth: 100 } CheckBox { id: checkBoxTime; } }
+						TextWidget { text: "End Time:" }
+						HorizontalLayout { EditLine { id: editLineEndTime; text: "0 0 0"; minWidth: 100; maxWidth: 100 } CheckBox { id: checkBoxEndTime; } }
+						TextWidget { text: "Duration:" }
+						EditLine { id: editLineDuration; text: "0 0 0"; minWidth: 100; maxWidth: 100 }
+						
+						Button { id: buttonSet; maxWidth: 100; text: "*Set" }
+
+					}
 				}
 			}
 		});
@@ -98,6 +107,52 @@ public:
 		_checkBoxTime = _window.mainWidget.childById!CheckBox("checkBoxTime");
 		_checkBoxEndTime = _window.mainWidget.childById!CheckBox("checkBoxEndTime");
 		
+		_window.mainWidget.childById!Button("buttonTest").click = delegate(Widget w) {
+			EditLine _editLineSpot;
+			
+			Window _spotWin;
+			
+			_spotWin = Platform.instance.createWindow(
+				"Command", null, WindowFlag.Resizable, 800, 50);
+
+			_spotWin.mainWidget = parseML(q{
+				HorizontalLayout {
+					backgroundColor: "#C0E0E070" // semitransparent yellow background
+					HorizontalLayout {
+						TextWidget {
+							text: "Enter command:"
+						}
+						EditLine {
+							id: editLineSpot
+							minWidth: 500
+						}
+						Button {
+							id: buttonAction
+							text: "Action"
+						}
+					}
+				}
+			});
+
+			_editLineSpot = _spotWin.mainWidget.childById!EditLine("editLineSpot");
+
+			_spotWin.mainWidget.childById!Button("buttonAction").click = delegate(Widget w) {
+				auto output = _control.processInput(_editLineSpot.text.to!string);
+
+				if (output.length > 0) {
+					_editBoxMain.text = _editBoxMain.text ~ (output ~ "\n").to!dstring;
+
+					return true;
+				}
+
+				return false;
+			};
+
+			_spotWin.show();
+
+			return true;
+		};
+
 		_window.mainWidget.childById!Button("buttonActivate").click = delegate(Widget w) {
 			import std.string: lastIndexOf;
 			import std.conv: to;
@@ -148,6 +203,56 @@ public:
 					_editLineDuration.text = text(task.timeLength.hours, " ", task.timeLength.minutes, " ", task.timeLength.seconds).to!dstring;
 
 				_editLineComment.text = task.comment().to!dstring;
+			}
+
+            return true;
+		};
+
+		/*
+		_window.mainWidget.childById!Button("buttonSet").click = delegate(Widget w) {
+			return false;
+		};
+		*/
+
+		_window.mainWidget.childById!Button("buttonSet").click = delegate(Widget w) {
+
+            int id;
+			try {
+				id = _editLineId.text.to!int;
+			} catch(Exception e) {
+				return false;
+			}
+
+			auto task = _taskMan.getTask(id); 
+			if (task is null) {
+				_editBoxMain.text = _editBoxMain.text ~ "\nTask out of range!";
+
+				return false;
+			} else {
+				/+
+				_textWidgetCategory.text = task.id().to!dstring ~ " " ~ task.taskString.to!dstring;
+
+				_editLineDate.text = text(task.dateTime.day, " ", cast(int)task.dateTime.month, " ", task.dateTime.year).to!dstring;
+
+				_checkBoxTime.checked = task.displayTimeFlag;
+				if (_checkBoxTime.checked)
+					_editLineTime.text = text(task.dateTime.hour, " ", task.dateTime.minute, " ", task.dateTime.second).to!dstring;
+				else
+					_editLineTime.text = ""d;
+
+				_checkBoxEndTime.checked = task.displayEndTimeFlag;
+				if (_checkBoxEndTime.checked)
+					_editLineEndTime.text = text(task.endTime.hour, " ", task.endTime.minute, " ", task.endTime.second).to!dstring;
+				else
+					_editLineEndTime.text = ""d;
+				
+				if (task.timeLength.hours ==0 && task.timeLength.minutes == 0 && task.timeLength.seconds == 0)
+					_editLineDuration.text = ""d;
+				else
+					_editLineDuration.text = text(task.timeLength.hours, " ", task.timeLength.minutes, " ", task.timeLength.seconds).to!dstring;
+
+				_editLineComment.text = task.comment().to!dstring;
+				+/
 			}
 
             return true;
